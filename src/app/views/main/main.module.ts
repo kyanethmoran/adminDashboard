00798d3996884ad2ef2c0777c691d5d53a299818
Your Name <you@example.com>
2023-09-08 22:59:53 -0400

    header & footer styled

--- src/app/views/main/main.module.ts
+++ src/app/views/main/main.module.ts
@@ -5,9 +5,10 @@ import { MainComponent } from './main.component';
 import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
 import { PostsComponent } from 'src/app/components/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [MainComponent, DashboardComponent, PostsComponent],
-  imports: [CommonModule, RouterModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule, MatSidenavModule],
 })
 export class MainModule {}
