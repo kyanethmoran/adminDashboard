8df847f17e017403d5a72b604636931f95f6e5d6
Your Name <you@example.com>
2023-09-09 00:20:01 -0400

    toggle sideNav

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {}
+export class MainComponent implements OnInit {
+  sideNavOpen = false;
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  sideNavToggler() {
+    this.sideNavOpen = !this.sideNavOpen;
+  }
+}
