8f05cc8f66e06cfcedb82bb0c5a3c7df58005d57
Your Name <you@example.com>
2023-09-12 22:49:24 -0400

    format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainModule } from './views/main/main.module';
 
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
