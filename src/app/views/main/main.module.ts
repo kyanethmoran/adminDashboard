3504419300758ea35510d3b1e8a73c053f3805bd
Your Name <you@example.com>
2023-09-13 14:43:09 -0400

    maintenance component created

--- src/app/views/main/main.module.ts
+++ src/app/views/main/main.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
 import { PostsComponent } from 'src/app/components/posts/posts.component';
+import { MaintenanceComponent } from 'src/app/components/maintenance/maintenance.component';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -12,7 +13,12 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  declarations: [MainComponent, DashboardComponent, PostsComponent],
+  declarations: [
+    MainComponent,
+    DashboardComponent,
+    PostsComponent,
+    MaintenanceComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule,
