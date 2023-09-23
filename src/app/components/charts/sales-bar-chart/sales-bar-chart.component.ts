030fffaf8181b4d487ee76c1ec3cc2cf7a0d870b
Your Name <you@example.com>
2023-09-23 18:26:07 -0400

    d3 bar chart for sales to date

--- src/app/components/charts/sales-bar-chart/sales-bar-chart.component.ts
+++ src/app/components/charts/sales-bar-chart/sales-bar-chart.component.ts
@@ -1,26 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
-import { HttpClient } from '@angular/common/http';
 import { SALES } from '../sales';
+
+// import * as d3 from 'd3-selection';
+import * as d3Scale from 'd3-scale';
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
 
 @Component({
   selector: 'app-sales-bar-chart',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './sales-bar-chart.component.html',
   styleUrls: ['./sales-bar-chart.component.scss'],
 })
-export class SalesBarChartComponent {
-  title = 'Bar Chart';
+export class SalesBarChartComponent implements OnInit {
+  title = 'Sales to Date';
 
-  private width: number = 650;
+  private width: number = 750;
   private height: number = 350;
-  private margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  private margin = { top: 20, right: 20, bottom: 30, left: 60 };
 
   private x: any;
   private y: any;
   private svg: any;
   private g: any;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
     this.initSvg();
@@ -45,9 +50,9 @@ export class SalesBarChartComponent {
   }
 
   initAxis() {
-    this.x = d3.scaleBand().rangeRound([0, this.width]).padding(0.1);
+    this.x = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
 
-    this.y = d3.scaleLinear().rangeRound([this.height, 0]);
+    this.y = d3Scale.scaleLinear().rangeRound([this.height, 0]);
 
     this.x.domain(SALES.map((d) => d.month));
     this.y.domain([0, d3.max(SALES, (d) => d.value)]);
@@ -58,18 +63,25 @@ export class SalesBarChartComponent {
       .append('g')
       .attr('class', 'axis axis--x')
       .attr('transform', 'translate(0,' + this.height + ')')
-      .call(d3.axisBottom(this.x));
+      .call(d3Axis.axisBottom(this.x))
+      .append('text')
+      .attr('class', 'axis-title')
+      .attr('font-size', '14px')
+      .attr('x', 330)
+      .attr('y', 30)
+      .attr('dx', '.65em')
+      .text('Month/Year');
 
     this.g
       .append('g')
       .attr('class', 'axis axis--y')
-      .call(d3.axisLeft(this.y).ticks(10))
+      .call(d3Axis.axisLeft(this.y).ticks(10))
       .append('text')
       .attr('class', 'axis-title')
       .attr('transform', 'rotate(-90)')
-      .attr('y', -40)
-      .attr('dy', '.71em')
-      .style('text-anchor', 'end')
+      .attr('y', -45)
+      .attr('dy', '0.71em')
+      .attr('text-anchor', 'end')
       .text('Sales (units)');
   }
 
@@ -83,6 +95,15 @@ export class SalesBarChartComponent {
       .attr('x', (d: any) => this.x(d.month))
       .attr('y', (d: any) => this.y(d.value))
       .attr('width', this.x.bandwidth())
-      .attr('height', (d: any) => this.height - this.y(d.value));
+      .transition()
+      .ease(d3.easeLinear)
+      .duration(500)
+      .delay(function (d: any, i: any) {
+        return i * 50;
+      })
+      .attr('height', (d: any) => this.height - this.y(d.value))
+      .append('title')
+      .attr('class', 'tooltip')
+      .text((d: any) => `${d.value} Units Sold in ${d.month}`);
   }
 }
